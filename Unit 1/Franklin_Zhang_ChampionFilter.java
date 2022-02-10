c3e5a1d82acae23dde8c2563c2541d4de97293a2
cyndaqui1 <31288194+cyndaqui1@users.noreply.github.com>
2022-02-10 10:03:44 -0500

    Add files via upload

---
+++ Unit 1/Franklin_Zhang_ChampionFilter.java
@@ -0,0 +1,68 @@
+/**
+ * Name: Franklin Zhang
+ * Date: 20222-02-08
+ * Description: Program to find the hp and champion with the highest hp, and the armor and champion with the lowest armor
+ */
+
+import java.util.*; // imports all moddules in util library
+import java.io.*; // imports all modules from io library
+
+public class Franklin_Zhang_ChampionFilter {
+	public static void main(String[] arg) {
+		
+
+		// declare empty arrays for champion names, hitpoints, and armor
+		String[] champions = new String[152];
+		double[] hp = new double[152];
+		double[] armor = new double[152];
+
+		int index = 0; // counter used for filling out respective indexes in arrays above
+		
+		try {
+
+			String line;
+			
+			//create an object to read from file "champions.json"
+			File file = new File("champions.json");
+			FileReader filereader = new FileReader(file);
+			BufferedReader reader = new BufferedReader(filereader);
+			
+			while ((line = reader.readLine()) != null) {
+
+				if (line.contains("\"id\"")) { // check if line contains string "id", then cuts the important information to fill in spot in champions name array
+					champions[index] = line.substring(line.indexOf(": \"") + 3,line.length()-2);
+				}
+
+				if (line.contains("\"hp\"")) { // check if line contains string "hp", then cuts the important information to fill in spot in hp array
+					hp[index] = Double.parseDouble(line.substring(line.indexOf(":") + 2, line.length()-1));
+				}
+				
+				if (line.contains("\"armor\"")) { // checks if line contains string "armor", then cuts the important information to fill in spot in armor array. Additionally, since its the last attribute of the respective champion needed, the index counter is incremented so the array progressively fills out
+					armor[index] = Double.parseDouble(line.substring(line.indexOf(":") + 2, line.length()-1));
+					index++;
+				}
+			}
+		}
+
+		catch (Exception e) {}
+
+		index = 0; // reset pointer
+
+		for (int i = 1; i < 152; i++) { // reuses index as a pointer, in which case, we are finding the largest element, and using the index as a pointer to that element
+
+			if (hp[i] > hp[index]) {
+				index = i;
+			}
+		}
+		System.out.println("Highest HP: " + champions[index] + " (" + hp[index] + "HP)"); // outputs largest hp champion's hp and name
+		for (int i = 1; i < 152; i++) { // finds the weakest armor and uses index variable as a pointer to the element
+
+			if (armor[i] < armor[index]) {
+				index = i;
+			}
+		}
+
+		System.out.println("Lowest Armor: " + champions[index] + " (" + armor[index] + " Armor)"); // outputs largest armor champion's hp and name
+
+	}
+}
---
+++ Unit 1/Franklin_Zhang_Encryption.java
@@ -0,0 +1,56 @@
+/**
+ * Name: Franklin Zhang
+ * Date: 20222-02-08
+ * Description: Program to encrypt user input with swapping first and last characters of words and operating ROT-2 on the characters in the middle
+ */
+
+import java.util.*; //imports all modules from library in java
+
+public class Franklin_Zhang_Encryption {
+	
+	static Scanner scanner = new Scanner(System.in); //declare a scanner object
+
+
+	/**
+	 *  Method to gather and filter input
+	 * 
+	 * @return returns the filtered input string
+	 */
+	public static String validate() { // used to gather input and filter
+
+		String input = scanner.nextLine(); // reads user input
+		
+		while (input.length() < 2) { // executes loop as long as input is no greater than 2 characters
+			input = scanner.nextLine(); // reads user input
+		}
+		
+		return input; // returns accepted input as string
+	}
+	public static void main(String[] arg) {
+
+		System.out.print("Hello, encrypt your string here: "); // prompt input from user
+		String raw = validate(); // calls validate module for input
+		String[] segmented = raw.split(" "); // splits input based on spaces existing
+		String encrypted = ""; // empty result, eventually append characters to make a full output
+
+		for (int i = 0, j = 0; i < segmented.length;i++) { // loops through array
+			if (segmented[i].length() == 0) { // if whitespaces are present in the input, the split would've added an empty string to the array, and which I will utilize to refill the whitespace in the encrypted text
+				encrypted += " "; // add a space
+				continue; // continue next iteration
+			}
+			encrypted += segmented[i].charAt(segmented[i].length()-1); // appends last character of segmented[i] to encrypted
+
+			for (int x = 1; x < segmented[i].length()-1;x++) { // loops through segmented[i] characters
+				encrypted += (char) (segmented[i].charAt(x) + 2); // executes rot2 on character and appends to string
+				j++;
+			}
+
+			encrypted += segmented[i].charAt(0); // appends first character of segmented[i]
+			encrypted += " ";// adds in space which were split from earlier
+		}
+
+
+		encrypted = encrypted.substring(0,encrypted.length()-1); // there will always be a whitespace at the end due to how the loop worked, so im shaving it
+		System.out.println(encrypted); // outputs encrypted string
+	}
+}
